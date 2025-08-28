1efcbb022718bfbf609e7d76f2b3f6f3c4ba109f
arbitrarymahi <iamaanjna@gmail.com>
2025-08-28 15:16:52 +0530

    solved: rotate matrix 90 degree

---
+++ rotateMatrix90.js
@@ -0,0 +1,29 @@
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const n = matrix.length; //nxn matrix
+
+  //transpose of matrix
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  //reverse matrix
+  for (let i = 0; i < n; i++) {
+    let j = 0;
+    while (j < n - 1 - j) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[i][n - 1 - j];
+      matrix[i][n - 1 - j] = temp;
+      j++;
+    }
+  }
+};
